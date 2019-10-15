beece29ffd0f033e44197a5923aab3b64ce14f55
Kyla Bendt <kylabendt@gmail.com>
2019-10-15 10:17:14 -0600

    add outline

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,12 @@
 	<title>Kyla PWP: Milestone 1</title>
 </head>
 <body>
+<h1>Kyla's PWP: Milestone 1</h1>
+<h2>Purpose, Audience, Goal</h2>
+<h3>Purpose</h3>
+<h3>Audience</h3>
+<h3>goal</h3>
+<h2>Persona</h2>
 
 </body>
 </html>
