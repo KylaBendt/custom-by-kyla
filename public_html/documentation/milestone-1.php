1f1b79a6275078b0287de8f6db35a0d6bb729d9e
Kyla Bendt <kylabendt@gmail.com>
2019-10-17 15:32:30 -0600

    Start filling in persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -14,6 +14,14 @@
 <h3>Goal</h3>
 <p>The goal is to provide product details, specs, pictures and compelling images and copy to make people buy step-stool/tables.</p>
 <h2>Persona</h2>
-</p>
+<p>Name: Sabina Morgan </p>
+<p>Age: 35</p>
+<p>Occupation: Runs a successful mommy blog about raising her 1 and 3 year-olds. </p>
+<p>Hobbies: jogging, photography, crafts</p>
+<p>Tech: A new Dell laptop and a Samsung S8.</p>
+<p>Tech skills/use: Knows how to use her tech fairly well. Uses Wordpress to blog, but doesn't know the techy details.  She spends a lot of time online and on social media to help grow her blog.</p>
+<p>Frustrations and needs: Sabina is a little overwhelmed by parenting and blogging.  She's always looking for things to make her life easier.  She's dedicated to being the best mom she can and wants to make sure she gives her kids the best possible start in life. </p>
+<p></p>
+<p>User Story: "As a user, I want to learn more about step-stools for kids so I can find a solution for my kids to be able to participate in the kitchen." </p>
 </body>
 </html>
