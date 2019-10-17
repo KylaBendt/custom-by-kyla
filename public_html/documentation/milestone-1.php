04fdc424d7ec6017af1bc48da0c814b2ab9ca97b
Kyla Bendt <kylabendt@gmail.com>
2019-10-17 15:28:49 -0600

    Add purpose, audience & goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,9 +8,12 @@
 <h1>Kyla's PWP: Milestone 1</h1>
 <h2>Purpose, Audience, Goal</h2>
 <h3>Purpose</h3>
+<p>The purpose of this site is to sell folding step-stool/tables for kids.</p>
 <h3>Audience</h3>
-<h3>goal</h3>
+<p>This site is designed for parents, grandparents or other people with young kids. </p>
+<h3>Goal</h3>
+<p>The goal is to provide product details, specs, pictures and compelling images and copy to make people buy step-stool/tables.</p>
 <h2>Persona</h2>
-
+</p>
 </body>
 </html>
