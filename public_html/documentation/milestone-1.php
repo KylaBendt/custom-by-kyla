e07188cad51f05a051719e86a50e76679511e2ac
Kyla Bendt <kylabendt@gmail.com>
2019-10-18 05:54:03 -0600

    flesh out perona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,25 +2,28 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Kyla PWP: Milestone 1</title>
+	<title>Custom by Kyla - Kyla's PWP: Milestone 1</title>
 </head>
 <body>
-<h1>Kyla's PWP: Milestone 1</h1>
+<h1>Custom by Kyla - Kyla's PWP: Milestone 1</h1>
 <h2>Purpose, Audience, Goal</h2>
 <h3>Purpose</h3>
-<p>The purpose of this site is to sell folding step-stool/tables for kids.</p>
+<p>The purpose of this site is to sell handmade furniture for kids.</p>
 <h3>Audience</h3>
 <p>This site is designed for parents, grandparents or other people with young kids. </p>
 <h3>Goal</h3>
-<p>The goal is to provide product details, specs, pictures and compelling images and copy to make people buy step-stool/tables.</p>
+<p>The goal is to provide product details, specs, pictures and compelling images and copy to make people buy customized, handmade, folding step-stool/tables for young children.</p>
 <h2>Persona</h2>
 <p>Name: Sabina Morgan </p>
 <p>Age: 35</p>
+<p>Relationship status: Married to Aaron for 7 years.</p>
 <p>Occupation: Runs a successful mommy blog about raising her 1 and 3 year-olds. </p>
 <p>Hobbies: jogging, photography, crafts</p>
 <p>Tech: A new Dell laptop and a Samsung S8.</p>
 <p>Tech skills/use: Knows how to use her tech fairly well. Uses Wordpress to blog, but doesn't know the techy details.  She spends a lot of time online and on social media to help grow her blog.</p>
-<p>Frustrations and needs: Sabina is a little overwhelmed by parenting and blogging.  She's always looking for things to make her life easier.  She's dedicated to being the best mom she can and wants to make sure she gives her kids the best possible start in life. </p>
+<p>Causes: Being good to the environment & supporting small businesses.  (She volunteered at the humane society before her kids were born.)</p>
+<p>Frustrations and needs: Sabina is a little overwhelmed by parenting and blogging.  She's always looking for things to make her life easier.  She needs something for her 1 year-old to stand on so he can 'help' when she's cooking.</p>
+<p>Goals: She's dedicated to being the best mom she can and wants to make sure she gives her kids the best possible start in life. </p>
 <p></p>
 <p>User Story: "As a user, I want to learn more about step-stools for kids so I can find a solution for my kids to be able to participate in the kitchen." </p>
 </body>
