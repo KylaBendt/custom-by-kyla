3d8a1641055e7c647907d808c3bcbb064b57a760
Kyla Bendt <kylabendt@gmail.com>
2019-10-30 10:05:12 -0600

    work on wireframe display

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,9 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Custom by Kyla - Kyla's PWP: Milestone 2</title>
+	<link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
-<h1>Wireframes - Custom by Kyla - Kyla's PWP: Milestone 2
+<h1>Wireframes - Custom by Kyla - Kyla's PWP: Milestone 2</h1>
+	<p>Mobile wireframe is based on a screen size of 375x812.  Each section is 770 pixels tall to show that there is another section at the bottom and encourage users to scroll. </p>
+	<p>Web wireframe  is based on a screen size of 1280x800.   Each section is 746 pixels tall.  </p>
+<div>
+	<img src="./images/kyla-mobile.png" alt="Wireframe for mobile">
 
+	<img src="./images/kyla-web.png" alt="Wireframe for web">
+</div>
 </body>
 </html>
