d59141515693221330236bf5d4b410b93ad7d485
Kyla Bendt <kylabendt@gmail.com>
2019-10-30 11:18:10 -0600

    align wireframes vertically

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,8 @@
 <body>
 <h1>Wireframes - Custom by Kyla - Kyla's PWP: Milestone 2</h1>
 	<p>Mobile wireframe is based on a screen size of 375x812.  Each section is 770 pixels tall to show that there is another section at the bottom and encourage users to scroll. </p>
-	<p>Web wireframe  is based on a screen size of 1280x800.   Each section is 746 pixels tall.  </p>
+	<p>Web wireframe  is based on a screen size of 1280x800.  Each section is 746 pixels tall.  </p>
+	<p>Sections of the site are separated by thin grey lines- this is for clarity on the wireframes only and will not be included on the final site. </p>
 <div>
 	<img src="./images/kyla-mobile.png" alt="Wireframe for mobile">
 
