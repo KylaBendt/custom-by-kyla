aba7059679153089803ed01dfcde634846c12c18
Kyla Bendt <kylabendt@gmail.com>
2019-10-30 09:35:37 -0600

    make milestone 2 page & add wireframe images

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Custom by Kyla - Kyla's PWP: Milestone 2</title>
+</head>
+<body>
+<h1>Wireframes - Custom by Kyla - Kyla's PWP: Milestone 2
+
+</body>
+</html>
