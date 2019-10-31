e99798ef3567de1173800a5d5750d9c94dcc3aac
Kyla Bendt <kylabendt@gmail.com>
2019-10-30 20:26:58 -0600

    add content strategy section

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,7 +6,22 @@
 	<link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
-<h1>Wireframes - Custom by Kyla - Kyla's PWP: Milestone 2</h1>
+<h1>Custom by Kyla - Kyla's PWP: Milestone 2</h1>
+<h2>Content Strategy</h2>
+<h3>First Impression</h3>
+<p>The first section of the site will be a large image with some simple title text on top.  </p>
+<h3>Images</h3>
+<p>The second section will feature a catchy headline and an image that will change every few seconds to showcase different pictures.  It will also have an 'order now' button that will take the user to the email form where they can contact me for an order.  </p>
+<h3>Safety section</h3>
+<p>Parents like to feel like their kids will be safe so his section will hilight safety features of the step stool to make parents feel at ease.  This will include a large title for the page, a bullet list of safety features and an image showcasing these. </p>
+<h3>Custom options</h3>
+<p>This section will focus on the customizable and hand-built nature of the product.  It will include title text, an image and a list of custom options.  It also have an order button (Call to action!).  In part, this is with the idea that the site could eventually be turned into a site where a user could actually make a purchase and I want the design to reflect that feel.</p>
+<h3>The construction process/ About the company (me)</h3>
+<p>This section will include an image carousel or gallery the user can go through to see images of the construction process or conveying the small-business aspect of the company.  It may also include some short text about the process and/or company.</p>
+<h3>Contact/Order Form</h3>
+<p>The last section will be the email contact form.  It will have text to indicate that the user can get a quote, start the customization process or initiate an email by filling out the form.  </p>
+
+<h2>Wireframes</h2>
 	<p>Mobile wireframe is based on a screen size of 375x812.  Each section is 770 pixels tall to show that there is another section at the bottom and encourage users to scroll. </p>
 	<p>Web wireframe  is based on a screen size of 1280x800.  Each section is 746 pixels tall.  </p>
 	<p>Sections of the site are separated by thin grey lines- this is for clarity on the wireframes only and will not be included on the final site. </p>
