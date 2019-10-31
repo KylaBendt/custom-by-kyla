907d7abb802a0209312f71c344dbc4a116d1a4af
Kyla Bendt <kylabendt@gmail.com>
2019-10-30 20:31:29 -0600

    small edits to text

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,7 +13,7 @@
 <h3>Images</h3>
 <p>The second section will feature a catchy headline and an image that will change every few seconds to showcase different pictures.  It will also have an 'order now' button that will take the user to the email form where they can contact me for an order.  </p>
 <h3>Safety section</h3>
-<p>Parents like to feel like their kids will be safe so his section will hilight safety features of the step stool to make parents feel at ease.  This will include a large title for the page, a bullet list of safety features and an image showcasing these. </p>
+<p>Parents like to feel like their kids will be safe so this section will highlight safety features of the step stool to make parents feel at ease.  This will include a large title for the page, a bullet list of safety features and an image showcasing these. </p>
 <h3>Custom options</h3>
 <p>This section will focus on the customizable and hand-built nature of the product.  It will include title text, an image and a list of custom options.  It also have an order button (Call to action!).  In part, this is with the idea that the site could eventually be turned into a site where a user could actually make a purchase and I want the design to reflect that feel.</p>
 <h3>The construction process/ About the company (me)</h3>
@@ -22,8 +22,7 @@
 <p>The last section will be the email contact form.  It will have text to indicate that the user can get a quote, start the customization process or initiate an email by filling out the form.  </p>
 
 <h2>Wireframes</h2>
-	<p>Mobile wireframe is based on a screen size of 375x812.  Each section is 770 pixels tall to show that there is another section at the bottom and encourage users to scroll. </p>
-	<p>Web wireframe  is based on a screen size of 1280x800.  Each section is 746 pixels tall.  </p>
+	<p>Each section is intended to be slightly shorter than the screen height so a small bit of the next section is visible encouraging users to scroll to see what is there.</p>
 	<p>Sections of the site are separated by thin grey lines- this is for clarity on the wireframes only and will not be included on the final site. </p>
 <div>
 	<img src="./images/kyla-mobile.png" alt="Wireframe for mobile">
