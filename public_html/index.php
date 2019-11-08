d6184bf23762366eaf09b23327139c7636e8959c
Kyla Bendt <kylabendt@gmail.com>
2019-11-07 19:42:48 -0700

    add carousel

--- public_html/index.php
+++ public_html/index.php
@@ -27,7 +27,7 @@
 	<title>Custom by Kyla</title>
 </head>
 <body>
-<!--Opening Section-->
+<!--Opening (Title) Section-->
 <section>
 	<div class="container-fluid top-image">
 		<div class="p-5 section-height">
@@ -47,11 +47,52 @@
 	</div>
 </section>
 
-<!--Third Second section-->
+<!--Third section-->
 <section>
 	<div class="container-fluid ">
-		<div class="section-height">
+		<div class="">
 			<h2>Involve your child!</h2>
+			<div class="bd-example">
+				<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+						<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+
+							<img src="https://www.livehoppy.com/wp-content/uploads/2019/04/kids-bunny-table.jpg" class="d-block w-100" alt="...">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Converts into a table!</h5>
+								<p>This two-in-one stool saves space and converts into a child-sized table.</p>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="https://www.livehoppy.com/wp-content/uploads/2019/04/B-on-stool.jpg" class="d-block w-100" alt="...">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>The kitchen helper</h5>
+								<p>Your child will love to be able to see what is going on.</p>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="https://www.livehoppy.com/wp-content/uploads/2019/04/kids-bunny-stool-2.jpg" class="d-block w-100" alt="...">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Custom made</h5>
+								<p>Made by a mom.</p>
+							</div>
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
